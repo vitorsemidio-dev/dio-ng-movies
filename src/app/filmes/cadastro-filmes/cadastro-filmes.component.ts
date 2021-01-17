4bb83e5e48780c8ae3fbff6e72c69dab32419e38
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-16 23:47:51 -0300

    🚧 chore: formulário html conectado ao FB

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -12,26 +12,21 @@ export class CadastroFilmesComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.cadastro = this.fb.group(
-      {
-        titulo: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(2),
-            Validators.maxLength(256),
-          ],
+    this.cadastro = this.fb.group({
+      titulo: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
         ],
-        urlFoto: ['', [Validators.minLength(10)]],
-        dtLancamento: ['', [Validators.required]],
-        descricao: [''],
-        nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
-        urlIMDb: ['', [Validators.minLength(10)]],
-        genero: ['', [Validators.required]],
-      },
-      {
-        updateOn: 'blur',
-      },
-    );
+      ],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]],
+    });
   }
 }
