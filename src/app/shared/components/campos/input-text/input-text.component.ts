a3a634c2b3b2c7d4036c4fba3534c2bd62f72866
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-17 02:17:49 -0300

    🚧 refactor: Input-text com BaseComponent

--- src/app/shared/components/campos/input-text/input-text.component.ts
+++ src/app/shared/components/campos/input-text/input-text.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { AbstractControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
+import { BaseComponent } from './../base/base.component';
 import { ValidarCamposService } from './../validar-campos.service';
 
 @Component({
@@ -8,18 +8,8 @@ import { ValidarCamposService } from './../validar-campos.service';
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.scss'],
 })
-export class InputTextComponent {
-  @Input() titulo: string;
-  @Input() controlName: string;
-  @Input() formGroup: FormGroup;
-
-  constructor(private validarCamposService: ValidarCamposService) {}
-
-  get formControl(): AbstractControl {
-    return this.formGroup.controls[this.controlName];
-  }
-
-  validarCampo(control: AbstractControl, errorName: string) {
-    return this.validarCamposService.hasErrorValidation(control, errorName);
+export class InputTextComponent extends BaseComponent {
+  constructor(public validarCamposService: ValidarCamposService) {
+    super(validarCamposService);
   }
 }
