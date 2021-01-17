bc3b99d311b53418fdfc7d2f8f32ae25afffb0b2
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-16 23:35:39 -0300

    🚧 chore: Criação FormBuilder para cadastro

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -7,14 +7,31 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./cadastro-filmes.component.scss'],
 })
 export class CadastroFilmesComponent implements OnInit {
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
-    });
+    this.cadastro = this.fb.group(
+      {
+        titulo: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(256),
+          ],
+        ],
+        urlFoto: ['', [Validators.minLength(10)]],
+        dtLancamento: ['', [Validators.required]],
+        descricao: [''],
+        nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+        urlIMDb: ['', [Validators.minLength(10)]],
+        genero: ['', [Validators.required]],
+      },
+      {
+        updateOn: 'blur',
+      },
+    );
   }
 }
