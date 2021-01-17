42ed1e118a6ee03c8b684413ebef474e1c3fbc1d
Vitor Emidio <vitorsemidio.96@gmail.com>
2021-01-17 01:48:04 -0300

    🚧 refactor: Implementação input-number

--- src/app/shared/components/campos/input-number/input-number.component.ts
+++ src/app/shared/components/campos/input-number/input-number.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ValidarCamposService } from './../validar-campos.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'dio-input-number',
   templateUrl: './input-number.component.html',
-  styleUrls: ['./input-number.component.scss']
+  styleUrls: ['./input-number.component.scss'],
 })
 export class InputNumberComponent implements OnInit {
+  @Input() titulo: string;
+  @Input() controlName: string;
+  @Input() formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private validarCamposService: ValidarCamposService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
+  validarCampo(control: AbstractControl, errorName: string) {
+    return this.validarCamposService.hasErrorValidation(control, errorName);
+  }
 }
